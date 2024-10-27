b93341812aa2e246448d67fb25953cec3fec3e3a
NormalDuck13 <floristhelolfox@gmail.com>
2024-10-27 16:33:30 +0100

    Delete public directory

--- public/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const settingsBtn = document.querySelector('.settings');
-    const body = document.body;
-
-    settingsBtn.addEventListener('click', () => {
-        body.classList.toggle('dark-mode');
-    });
-});
